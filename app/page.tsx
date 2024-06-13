b62ed9377696b6748e5187cca13b0a347c05f2a4
yucchi-p <yusuke.sato.0214@gmail.com>
2024-06-13 16:58:46 +0900

    change

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,8 @@ export default function App() {
         </a>
       </div>
       <script type="text/javascript">
-        (function(w, d, x, id){
+        {`
+          (function(w, d, x, id){
           s=d.createElement('script');
           s.src='https://dtn7rvxwwlhud.cloudfront.net/amazon-connect-chat-interface-client.js';
           s.async=1;
@@ -59,6 +60,7 @@ export default function App() {
         amazon_connect('styles', { iconType: 'CHAT_VOICE', openChat: { color: '#ffffff', backgroundColor: '#123456' }, closeChat: { color: '#ffffff', backgroundColor: '#123456'} });
         amazon_connect('snippetId', 'QVFJREFIaWFZYXRVSlpIekdkUUg5YXhZenVQMktKRXNIWTVFQWpBYVErTEdzRnpvZHdHS3dRZEhDc1haVE82OXBQSDlxMGlVQUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNU0liQlMzeVZUczdrbTFmOUFnRVFnQ3RuVG9mNWpmU1N2Z3ZMWVU2QmdOb0RuN1NKL0FMcWpENzVuSElDeTRlZEFhU0NxK2QzcG9tZGVCeWM6OlJJU3JkME1yVUtYaFErYTlyMCtiWVJNeFJ0T1JiUG1VNDh3cjB2U2JFdEtsVmh4dGdBdVZlVkNmUGNVMHl4dWo5NVdXTlBYSjVUM0ZhQjd0SExYSGRteW45WGpTTUZISU1RdW12bWpjM1Yxeko0d1dEaDZ5MURXTmxCZTRtVUVOM1Ryak1RUnVWMjZvV1loUjJTOUM4UUNjU0FHQkxmQT0=');
         amazon_connect('supportedMessagingContentTypes', [ 'text/plain', 'text/markdown', 'application/vnd.amazonaws.connect.message.interactive', 'application/vnd.amazonaws.connect.message.interactive.response' ]);
+        `}
       </script>
     </main>
   );
